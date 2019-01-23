3d5a41cff6f485919178e4dcdaae365c90583071
Anderson Ismael <23392311+aicoutodasilva@users.noreply.github.com>
2019-01-23 14:36:39 -0200

    A  3dimensoes85.jpg A  3dimensoes85.png A  4mundos.swf A  "Livro 1 Junto.pdf" A  "Livro 2 Junto.pdf" A  RaioB.swf A  Scripts/AC_RunActiveContent.js.pagespeed.jm.tb4ZozSNkq.js A  as-sefirot53.jpg A  as-sefirot53.png A  ccsy118a.jpg A  ccsy118a.png A  ccsy119a.jpg A  ccsy119a.png A  ccsy119a.png.1 A  ccsy121a.jpg A  ccsy121a.png A  ccsy14-l.png A  ccsy14.png A  ccsy15-l.png A  ccsy15.png A  ccsy16-l.png A  ccsy16.png A  ccsy22-l.png A  ccsy24.jpg A  ccsy24.png A  ccsy33-l.png A  ccsy33.jpg A  ccsy34-l.png A  ccsy34.jpg A  ccsy57.png A  ccsy6.jpg A  ccsy64-slide78.png A  ccsy68img.png A  ccsy70-l.jpg A  ccsy70.jpg A  ccsy73.png A  ccsy74A.png A  ccsy74B.png A  ccsy8.jpg A  foguinhoG.swf A  index.php A  index.swf A  kabbalah268.jpg A  kabbalah268.png A  qabalah120.jpg A  qabalah120.png A  slide100Ar.swf A  slide100r.swf A  slide101Ar.swf A  slide101r.swf A  slide104Nr.swf A  slide105Nr.swf A  slide106Nr.swf A  slide107Nr.swf A  slide108Nr.swf A  slide109Nr.swf A  slide110Nr.swf A  slide112.swf A  slide112.swf.1 A  slide113.swf A  slide113.swf.1 A  slide114.swf A  slide115.swf A  slide116.swf A  slide117.swf A  slide118.swf A  slide119.swf A  slide120.swf A  slide121.swf A  slide122.swf A  slide122.swf.1 A  slide122.swf.2 A  slide123.swf A  slide123.swf.1 A  slide124.swf A  slide125.swf A  slide125.swf.1 A  slide126.swf A  slide128A.swf A  slide26.swf A  slide33.swf A  slide34.swf A  slide35.png A  slide37.png A  slide39.png A  slide44.swf A  slide45.swf A  slide46.png A  slide48.swf A  slide50_N.swf A  slide50_N2.swf A  slide54.swf A  slide59_N.swf A  slide60_N.swf A  slide61.jpg A  slide61.png A  slide61_N.swf A  slide65+.swf A  slide65.swf A  slide66+.swf A  slide66r.swf A  slide67+.swf A  slide67A.swf A  slide67r.swf A  slide72.png A  slide77G.swf A  slide78.png A  slide78_A.png A  slide86.swf A  slide96.swf A  slide96_A.swf A  slide97Ar.swf A  slide97r.swf A  slide98Ar.swf A  slide98r.swf A  slide99Ar.swf A  slide99r.swf A  whois.txt A  xmlcomments.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>Séfer Yetziráh</title>
+<meta name="title" content="Séfer Yetziráh"/>
+<meta HTTP-EQUIV="EXPIRES" CONTENT="0"/>
+<meta name="resource-type" content="flash"/>
+<meta name="subject" content="Interpretação do Livro Séfer Yetziráh"/>
+<meta name="revisit-after" content="2 days"/>
+<meta name="robots" content="index, follow"/>
+<meta name="googlebot" content="index, follow"/>
+<meta name="audience" content="all"/>
+<meta name="language" content="pt-br"/>
+<meta name="distribution" content="global"/>
+<meta name="generator" content="FAPnet"/>
+<meta name="author" content="http://www.fapnet.com.br"/>
+<meta name="autor" content="http://www.fapnet.com.br"/>
+<meta name="keywords" content="sefer yet zirah seferyet seferyetzirah sfer yetzirah livro estudo Abraham Golem Séfer Yetziráh"/>
+<meta name="description" content="De autoria atribuída ao patriarca Abraham, o Séfer Yetziráh é, sem dúvida, o mais antigo e misterioso de todos os textos que tratam da criação. Adotado pelo misticismo judaico, deu origem à cabala e a várias outras crenças e lendas, tais como a criação de um Golem - criatura produzida de barro na forma humana, que recebe vida graças ao poder mágico de textos e palavras recitadas pelos sábios a partir do Séfer Yetziráh. Atribui-se também ao Séfer Yetziráh a busca de um nome secreto de Deus, que daria ao seu descobridor poderes milagrosos. Conta-se que, dado ao seu valor histórico e toda a mística que cobre este texto, que o Vaticano tem em sua biblioteca uma versão manuscrita do século X ou XI, talvez a cópia mais antiga no mundo moderno. Provavelmente por pertencer no passado a algum tipo de tradição secreta que não permitia a escrita de textos sagrados para protegê-los de interresses mundanos, o surgimento dos primeiros textos apresentaram conteúdos diferentes com relação às simbologias e também ao de número de palavras utilizadas. Até nos dias atuais os rabinos advertem sobre os riscos graves de perda de sanidade mental ao se praticar a leitura do Séfer Yetziráh sozinho. A leitura, segundo eles, deve ser praticada no mínimo com duas pessoas e preferivelmente uma delas deve ser versada no assunto. O texto dita, de uma forma bastante direta, que de tudo o que foi criado e tudo o que esta para ser criado o será sempre por meio de 10 emanações combinadas com as vinte e duas letras do alfabeto hebraico. Para ler o texto clique sobre o Livro."/>
+<script src="Scripts/AC_RunActiveContent.js.pagespeed.jm.tb4ZozSNkq.js" type="text/javascript"></script>
+<style type="text/css">body{margin-left:0;margin-top:0;margin-right:0;margin-bottom:0}</style>
+</head>
+<body>
+<div style="width:100%; height:100%; position:absolute;">
+<script type="text/javascript">AC_FL_RunContent('codebase','http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,28,0','width','100%','height','100%','align','absmiddle','title','Kabalah','src','index','quality','high','pluginspage','http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash','movie','index');</script>
+<noscript>
+<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,28,0" width="100%" height="100%" align="absmiddle" title="Kabalah">
+<param name="movie" value="index.swf"/>
+<param name="quality" value="high"/>
+<embed src="index.swf" width="100%" height="100%" align="absmiddle" quality="high" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash"></embed>
+</object>
+</noscript>
+</div>
+<script data-pagespeed-no-defer type="text/javascript">//<![CDATA[
+(function(){function f(b){var a=window;if(a.addEventListener)a.addEventListener("load",b,!1);else if(a.attachEvent)a.attachEvent("onload",b);else{var c=a.onload;a.onload=function(){b.call(this);c&&c.call(this)}}};window.pagespeed=window.pagespeed||{};var k=window.pagespeed;function l(b,a,c,g,h){this.h=b;this.i=a;this.l=c;this.j=g;this.b=h;this.c=[];this.a=0}l.prototype.f=function(b){for(var a=0;250>a&&this.a<this.b.length;++a,++this.a)try{null!=document.querySelector(this.b[this.a])&&this.c.push(this.b[this.a])}catch(c){}this.a<this.b.length?window.setTimeout(this.f.bind(this),0,b):b()};
+k.g=function(b,a,c,g,h){if(document.querySelector&&Function.prototype.bind){var d=new l(b,a,c,g,h);f(function(){window.setTimeout(function(){d.f(function(){for(var a="oh="+d.l+"&n="+d.j,a=a+"&cs=",b=0;b<d.c.length;++b){var c=0<b?",":"",c=c+encodeURIComponent(d.c[b]);if(131072<a.length+c.length)break;a+=c}k.criticalCssBeaconData=a;var b=d.h,c=d.i,e;if(window.XMLHttpRequest)e=new XMLHttpRequest;else if(window.ActiveXObject)try{e=new ActiveXObject("Msxml2.XMLHTTP")}catch(g){try{e=new ActiveXObject("Microsoft.XMLHTTP")}catch(h){}}e&&
+(e.open("POST",b+(-1==b.indexOf("?")?"?":"&")+"url="+encodeURIComponent(c)),e.setRequestHeader("Content-Type","application/x-www-form-urlencoded"),e.send(a))})},0)})}};k.criticalCssBeaconInit=k.g;})();
+pagespeed.selectors=["body"];pagespeed.criticalCssBeaconInit('/mod_pagespeed_beacon','http://sepheryetzirah.com.br/','q6KUV55pBD','hGkYDtDVE1g',pagespeed.selectors);
+//]]></script></body>
+</html>
+
---
+++ xmlcomments.php
@@ -0,0 +1,9 @@
+<?xml version="1.0"?>
+<thread id="Sefer Yetzirah" created="14:54:04 04/04/2008" cnt="0"><comment id="c_0" sender="ANDRE@GRUPOSAFARI.COM.BR" name="ANDRE ZOGBI" date="14:54:04 04/04/2008" subject="TESTE" flag="1"><message>TESTE</message></comment><comment id="c_0" sender="alboberro@yahoo.com.br" name="Albo" date="19:30:34 04/06/2008" subject="parabens" flag="1"><message>`Parabens pelo excelente trabalho!!!</message></comment><comment id="c_0" sender="monicafisio@uol.com.br" name="monica" date="10:13:37 08/06/2008" subject="Entendendo a CRIA&#xC7;&#xC3;O" flag="1"><message>Ap&#xF3;s os 19 anos que eu acompanho sua vida e seus estudos,hoje eu concluo que DEUS  criou voc&#xEA; com potencial para   entender SUA CRIA&#xC7;&#xC3;O, e, transmiti-LA. Com amor, Monica</message></comment><comment id="c_0" sender="smcunha@yahoo.com" name="sergio marques da Cunha" date="13:07:00 09/06/2008" subject="opini&#xE3;o" flag="1"><message>Caro David Alves;
+Voce vizitou o site contemplativo e deixou a  sua opini&#xE3;o que agrade&#xE7;o.
+Sou brasileiro e vivo em Portugal, tambem sou profissional de vendas.
+Muito importante a divulga&#xE7;&#xE3;o da Kabala.
+Fa&#xE7;o a minha divulga&#xE7;&#xE3;o atraves do Tarot aqui em Portugal.
+Vamos trocar opini&#xF5;es?
+Sauda&#xE7;&#xF5;es;
+Smcunha.</message></comment><comment id="c_0" sender="kimalf@gmail.com" name="Joaquim Barreira" date="15:14:05 13/07/2008" subject="Cabala" flag="1"><message>Verifiquei que estamos sintonizados, o que me deixou sensibilizado. Seu trabalho &#xE9; um valioso contributo, para os estudiosos, que desejam aprofundar e cimentar os seus conhecimentos. Gostaria imenso do seu coment&#xE1;rio do meu livro "Teoria Astral" . Parab&#xE9;ns!  </message></comment></thread>
